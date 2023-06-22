35e60030fa40fb910812bf8dd57eebc746d9ae0f
Giancarlo Naranjo <giancarlo.naranjo@epn.edu.ec>
2023-06-22 18:42:55 -0500

    Nuevos cambios en react

---
+++ 03-react/my-app/app/componentes/c_menu.component.tsx
@@ -0,0 +1,12 @@
+// app/componentes/c_menu.component.tsx
+export default function C_menuComponent(){
+    return (
+        <>
+            <ul>
+                <li><a href="/">Inicio</a></li>
+                <li><a href="/a_hola_mundo">A Hola mundo</a></li>
+                <li><a href="/b_hola_mundo">B Hola mundo</a></li>
+            </ul>
+        </>
+    )
+}
